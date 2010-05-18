496cc0608f0495a7302749a13174b17f59404478
Darren Shafae <dshafae@papercheck.com>
2010-05-18 14:47:17 -0700

    Completed the Ruby Blocks lesson; discovered a way to pass multiple parameters back to a class and execute them without defining a method to specifically handle the sent parameters.

--- 12_blocks/machine.rb
+++ 12_blocks/machine.rb
@@ -1,7 +1,18 @@
 # machine.rb
+
+
 class Machine
-  def do_something
-     "hello"
-     @result = 7
+  def do_something(*args)    
+    c = 0
+    args.count == 0? loop = 0 : loop = 1
+      if loop == 1
+        args[0].times do |x|
+          c = yield         
+        end
+      else
+        c= yield
+      end
+    return c
   end
 end
+
